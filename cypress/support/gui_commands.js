8021a07d9631621241e644e7588e7e7f880dd143
Montanher <gabrielmontanherr@gmail.com>
2024-07-09 00:39:38 -0300

    updates

---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,53 @@
+//login
+
+Cypress.Commands.add('login', (
+  user = Cypress.env('user_name'),
+  password = Cypress.env('user_password'),
+  { cacheSession = true } = {},
+) => {
+  const login = () => {
+    cy.visit('/users/sign_in')
+
+    cy.get("[data-qa-selector='login_field']").type(user)
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
+    cy.get("[data-qa-selector='sign_in_button']").click()
+  }
+
+  const options = {
+    cacheAcrossSpecs: true,
+  }
+
+  if (cacheSession) { // otimização de salvar a sessão do usuário (cache)
+    cy.session(user, login, options)
+  } else {
+    login()
+  }
+})
+  
+  // logout 
+
+  Cypress.Commands.add('logout', () => {
+    cy.get('.qa-user-avatar').click()
+    cy.contains('Sign out').click()
+  })
+  
+  // create project
+
+  Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+  
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
+  })
+
+  // issue 
+
+  Cypress.Commands.add('gui_createIssue', issue => {
+    cy.visit(`/${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+  
+    cy.get('.qa-issuable-form-title').type(issue.title)
+    cy.get('.qa-issuable-form-description').type(issue.description)
+    cy.contains('Submit issue').click()
+  })
